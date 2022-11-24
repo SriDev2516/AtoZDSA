587429756541302c59c92caac5f215a40f523097
Srinivas_Annavarpu <Srinivas.Annavarpu@dellteam.com>
2022-11-24 16:43:16 -0600

    Find peak in mountain array

---
+++ PeakIndexInAnMountainArray/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PeakIndexInAnMountainArray
+{
+    class Program
+    {
+        //https://leetcode.com/problems/peak-index-in-a-mountain-array/
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
